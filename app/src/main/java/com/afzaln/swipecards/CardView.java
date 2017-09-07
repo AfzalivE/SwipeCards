c2237b10f84ab2d0a28d64ff696f4a3afa01ee27
Afzal Najam <afzalnaj@gmail.com>
2017-09-07 18:55:17 -0400

    Snapback, swipe right and left

--- app/src/main/java/com/afzaln/swipecards/CardView.java
+++ app/src/main/java/com/afzaln/swipecards/CardView.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.Toast;
 
 /**
  * Created by afzal on 2017-09-06.
@@ -12,11 +14,15 @@ import android.view.View;
 
 public class CardView extends View {
     public static final int MAX_ROTATION = 30;
+    public static final double PROGRESS_THRESHOLD = 0.3;
 
     private float dx;
     private float dy;
     private float initialX;
     private float initialY;
+
+    private float lastProgress;
+    private boolean isSwipingAway;
 
     public CardView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -36,17 +42,39 @@ public class CardView extends View {
             case MotionEvent.ACTION_DOWN:
                 dx = getX() - event.getRawX();
                 dy = getY() - event.getRawY();
+                lastProgress = 0;
                 break;
             case MotionEvent.ACTION_UP:
+                float newX = event.getRawX() + dx;
+                float dragDistance = newX - initialX;
+                int screenWidth = getResources().getDisplayMetrics().widthPixels;
+                float progress = Math.min(Math.max(dragDistance / screenWidth, -1), 1);
+
+                if (isSwipingAway) {
+                    if (progress > PROGRESS_THRESHOLD) {
+                        swipeRight();
+                        Toast.makeText(getContext(), "Swipe right", Toast.LENGTH_SHORT).show();
+                        break;
+                    } else if (progress < -PROGRESS_THRESHOLD) {
+                        swipeLeft();
+                        Toast.makeText(getContext(), "Swipe left", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                }
+
+                resetPosition();
+
                 break;
             case MotionEvent.ACTION_MOVE:
-                float newX = event.getRawX() + dx;
+                newX = event.getRawX() + dx;
                 float newY = event.getRawY() + dy;
 
-                int screenWidth = getResources().getDisplayMetrics().widthPixels;
+                screenWidth = getResources().getDisplayMetrics().widthPixels;
 
-                float dragDistance = newX - initialX;
-                float progress = Math.min(Math.max(dragDistance / screenWidth, -1), 1);
+                dragDistance = newX - initialX;
+                progress = Math.min(Math.max(dragDistance / screenWidth, -1), 1);
+                isSwipingAway = Math.abs(progress) > Math.abs(lastProgress);
+                lastProgress = progress;
 
                 animate()
                         .x(newX)
@@ -61,5 +89,35 @@ public class CardView extends View {
 
         return true;
 
+    }
+
+    private void resetPosition() {
+        animate()
+                .x(initialX)
+                .y(initialY)
+                .rotation(0)
+                .setDuration(200)
+                .setInterpolator(new AccelerateDecelerateInterpolator())
+                .start();
+    }
+
+    private void swipeLeft() {
+        int screenWidth = getResources().getDisplayMetrics().widthPixels;
+
+        animate()
+                .x(-screenWidth)
+                .rotation(MAX_ROTATION)
+                .alpha(0f)
+                .setDuration(150);
+    }
+
+    private void swipeRight() {
+        int screenWidth = getResources().getDisplayMetrics().widthPixels;
+
+        animate()
+                .x(screenWidth)
+                .rotation(MAX_ROTATION)
+                .alpha(0f)
+                .setDuration(150);
     }
 }
